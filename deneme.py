a0b017bd4cdb73ca843541f963a8c403ee630a58
Enes Ağırman <99555923+EnesAgirman@users.noreply.github.com>
2023-11-21 07:10:50 +0300

    fccjj

--- deneme.py
+++ deneme.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 # Implementation of Explore-Then-Commit algorithm for arm 1 and arm 2 with beurnoulli 
 # distributions with 0.4 and 0.8 means respectively using 1000 trials and 500 time steps
@@ -17,12 +18,23 @@ def explore_then_commit_2_arm(arm1_mean, arm2_mean, T, N):
     
     K = 2   # number of arms
     
+    bound_1 = 0
+    bound_2 = 0
+    
+    delta = 0.1
+    
+    # initialize the number of trials for each arm for total in both exploration and exploitation phases
+    N_1 = 0
+    N_2 = 0
+    
     armMeans = np.zeros(K)
     armMeans[0] = arm1_mean
     armMeans[1] = arm2_mean
     
-    # initialize the total reward for each arm
-    totalReward = np.zeros(K)   
+    
+    armSamples = np.zeros(K)    # initialize the total reward for each arm
+    
+    armSampleMeans = np.zeros(K)    # initialize the sample means of each arm
     
     # initialize the means of each arm in the exploration phase
     explorationMeans = np.zeros(K)
@@ -31,23 +43,32 @@ def explore_then_commit_2_arm(arm1_mean, arm2_mean, T, N):
     totalAverageReward = 0
     
     ## Exploration Phase
-    totalReward[0] += bernoulliTrial(arm1_mean, N)
-    totalReward[1] += bernoulliTrial(arm2_mean, N)
+    armSamples[0] += bernoulliTrial(arm1_mean, N)
+    armSamples[1] += bernoulliTrial(arm2_mean, N)
     # Note: I was getting the results the double of what it should be so I divided N by 2 which solved the problem
     
     ## Exploitation Phase
     
     # choose the arm with the highest average in the exploration phase as the greedy arm
-    explorationMeans[0] = totalReward[0] / N
-    explorationMeans[1] = totalReward[1] / N
+    explorationMeans[0] = armSamples[0] / N
+    explorationMeans[1] = armSamples[1] / N
     
     greedyArm = np.argmax(explorationMeans)
     
-    totalReward[greedyArm] += bernoulliTrial(armMeans[greedyArm], T-N-N)
+    N_1 = N + (greedyArm == 0) * (T - 2*N)
+    N_2 = N + (greedyArm == 1) * (T - 2*N)
     
-    totalAverageReward = np.sum(totalReward) / T
+    bound_1 = math.sqrt( 1*math.log(K*T/(K*delta)) / N_1 )
+    bound_2 = math.sqrt( 0.06*math.log(K*T/(K*delta)) / N_2 )
     
-    return totalAverageReward
+    armSamples[greedyArm] += bernoulliTrial(armMeans[greedyArm], T-N-N)
+    
+    totalAverageReward = np.sum(armSamples) / T
+    
+    armSampleMeans[0] = armSamples[0] / N_1
+    armSampleMeans[1] = armSamples[1] / N_2
+    
+    return totalAverageReward, armSampleMeans, bound_1, bound_2, N_1, N_2
 
 def bernoulliTrial(p, n):
     """ do n beurnoulli trials with probability p and return the total reward
@@ -88,11 +109,29 @@ def main():
     
     num = 1000  # number of repetitions to be made
     
+    arm_1_bound_count = 0
+    arm_2_bound_count = 0
+    
+    N_1 = 0
+    N_2 = 0
+    
+    boundCount = np.zeros((len(N_array), 2))
+    
     for j in range(num):
         for i in range(len(N_array)):
-            totalRewardsArray[i] += explore_then_commit_2_arm(0.4, 0.8, 500, N_array[i])    # set N_array[i]//2 instead of N_array[i] to get similar results to part a of Q1
+            temp, armSampleMeans, bound_1, bound_2, N_1, N_2 = explore_then_commit_2_arm(0.4, 0.8, 500, N_array[i])    # set N_array[i]//2 instead of N_array[i] to get similar results to part a of Q1
+            if armSampleMeans[0] < 0.4 + bound_1 and armSampleMeans[0] > 0.4 - bound_1 and armSampleMeans[1] < 0.8 + bound_2 and armSampleMeans[1] > 0.8 - bound_2:
+                totalRewardsArray[i] += temp
+            if armSampleMeans[0] < 0.4 + bound_1 and armSampleMeans[0] > 0.4 - bound_1:
+                arm_1_bound_count += 1
+                boundCount[i][0] += 1
+            if armSampleMeans[1] < 0.8 + bound_2 and armSampleMeans[1] > 0.8 - bound_2:
+                arm_2_bound_count += 1
+                boundCount[i][1] += 1 
     
-    
+    print(f"arm1 was in the bound with probability % {100 * boundCount[4][0] / num }")
+    print(f"arm2 was in the bound with probability % {100 * boundCount[4][1] / num }")
+                
     totalAverageRewardArray = totalRewardsArray / num
     
     totalRegretArray = 0.8 * np.ones(len(N_array)) - totalAverageRewardArray
--- deneme2.py
+++ deneme2.py
@@ -1,45 +1,6 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
-# Function to simulate Explore & Commit Algorithm for a specific exploration length (N)
-def explore_commit_algorithm(N, num_trials=1000, T=500, true_means=[0.4, 0.8]):
-    total_rewards = np.zeros(num_trials)
 
-    for trial in range(num_trials):
-        # Placeholder code for Explore & Commit Algorithm
-        arm_rewards = np.zeros(2)  # Placeholder for storing rewards for each arm
-        chosen_arms = []  # Store the arms chosen during exploration phase
+boundProbabilities = np.zeros((5, 2))
 
-        for t in range(N):
-            # Exploration phase
-            arm = np.random.choice(2)  # Placeholder - randomly choose an arm
-            chosen_arms.append(arm)
-            reward = np.random.binomial(1, true_means[arm])  # Placeholder - sample reward from chosen arm
-            arm_rewards[arm] += reward
-
-        # Commit to the arm with the highest observed reward during exploration
-        chosen_arm = np.argmax(arm_rewards)
-        for t in range(N, T):
-            # Exploitation phase
-            reward = np.random.binomial(1, true_means[chosen_arm])  # Placeholder - sample reward from chosen arm
-            total_rewards[trial] += reward
-
-    avg_total_rewards = np.mean(total_rewards)
-    return avg_total_rewards
-
-# Specify exploration lengths (N)
-exploration_lengths = [1, 6, 11, 16, 21, 26, 31, 41, 46]
-
-# Simulate and plot results
-average_rewards = []
-for N in exploration_lengths:
-    avg_rewards = explore_commit_algorithm(N)
-    average_rewards.append(avg_rewards)
-
-# Plotting
-plt.plot(exploration_lengths, average_rewards, marker='o')
-plt.xlabel('Exploration Length (N)')
-plt.ylabel('Average Total Rewards')
-plt.title('Explore & Commit Algorithm Performance')
-plt.xticks(np.arange(1, 47, step=5))
-plt.show()
+print(boundProbabilities[4])
